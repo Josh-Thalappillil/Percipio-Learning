cd6192bd3aed2c31102d012f3a3595195f9e2e3f
Josh <jthalappillil7@gmail.com>
2023-03-17 14:46:15 +1100

    Updated
    updated files whilst learning on percipio

---
+++ JavaScript/JS Functions and Arrays/concat arrays/concat.js
@@ -0,0 +1,17 @@
+"use strict"
+
+let europeOffices = ['Bucharest', 'Prague', 'Rome'];
+let africaOffices = ['Durban', 'Nairobi'];
+let oceaniaOffices = ['Christchurch', 'Sydney'];
+
+let southernOffices = africaOffices.concat(oceaniaOffices);
+
+
+
+// myArray = [23,24,25];
+
+// let x = Math.min(32,...myArray, 21);
+
+
+// console.log(x)
+
