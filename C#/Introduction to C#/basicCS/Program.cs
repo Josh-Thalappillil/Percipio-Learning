13af547d124e1a9b8d817b637e0f29edbb3fd28b
Josh <jthalappillil7@gmail.com>
2023-03-20 12:47:46 +1100

    Testing variables in C#
    Tested manipulating variables in C# and then reviewing the output in console.

--- C#/Introduction to C#/basicCS/Program.cs
+++ C#/Introduction to C#/basicCS/Program.cs
@@ -3,13 +3,27 @@
 
 class Program{
     static void Main(string[] args){
+        // string firstName;
+        // firstName = "Josh";
+        // Console.WriteLine(firstName);
+        // firstName = "Alisha";
+        // Console.WriteLine(firstName);
+        // firstName = "Sienna";
+        // Console.WriteLine(firstName);
+        // The above is testing for interchanging variables
+
+        // Testing variables in console output below
         string firstName;
         firstName = "Josh";
-        Console.WriteLine(firstName);
-        firstName = "Alisha";
-        Console.WriteLine(firstName);
-        firstName = "Sienna";
-        Console.WriteLine(firstName);
+
+        int mailNum;
+        mailNum = 27;
+
+        decimal tempCelsius;
+        tempCelsius = 29.3m;
+
+        Console.WriteLine($"Hello, {firstName}! You have {mailNum} in your inbox. The temperature is {tempCelsius} celsius.");
+
     }
 }
 
