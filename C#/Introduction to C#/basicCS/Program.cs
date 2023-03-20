c776953419c2cf1fc6668db828042ee9a9141621
Josh <jthalappillil7@gmail.com>
2023-03-20 16:11:37 +1100

    Completed exercise for C# formatting
    Learn how to format console outputs and include character escapes, verbatim strings, unicode and string interpolation.

--- C#/Introduction to C#/basicCS/Program.cs
+++ C#/Introduction to C#/basicCS/Program.cs
@@ -12,7 +12,7 @@ class Program{
         // Console.WriteLine(firstName);
         // The above is testing for interchanging variables
 
-        // Testing variables in console output below
+        // Testing variables in console output below (Exercise 1)
         string firstName;
         firstName = "Josh";
 
@@ -24,7 +24,20 @@ class Program{
 
         Console.WriteLine($"Hello, {firstName}! You have {mailNum} in your inbox. The temperature is {tempCelsius} celsius.");
 
+        // Exercise 2
+        string projectName = "ACME";
+
+        string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+
+        Console.WriteLine($@"View English output:
+        c:\Exercise\{projectName}\data.txt");
+
+        Console.WriteLine($@"{russianMessage}:
+        c:\Exercise\{projectName}\ru-RU\data.txt");
+
     }
 }
+
 
 
