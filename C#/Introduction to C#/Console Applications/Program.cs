eadbd119bc161576f450d8b2448e2244b85bf9a6
Josh <jthalappillil7@gmail.com>
2023-03-22 10:55:25 +1100

    Update Program.cs
    Updated spelling

--- C#/Introduction to C#/Console Applications/Program.cs
+++ C#/Introduction to C#/Console Applications/Program.cs
@@ -3,7 +3,7 @@
 class Program{
     static void Main(string[]args){
         
-        // Code CHallenge: Find a function that returns the larger of two numbers(Example code below)
+        // Code Challenge: Find a function that returns the larger of two numbers(Example code below)
         // int firstValue = 500;
         // int secondValue = 600;
         // int largerValue;
