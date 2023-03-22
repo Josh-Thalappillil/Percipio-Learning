30e65e2841340131a910944b3c834485860587e3
Josh <jthalappillil7@gmail.com>
2023-03-22 15:47:21 +1100

    Completed challenge
    Completed challenge. Added decision logic and tested console.

--- C#/Introduction to C#/Guided Projects/Renewal Rate/Program.cs
+++ C#/Introduction to C#/Guided Projects/Renewal Rate/Program.cs
@@ -3,3 +3,36 @@
 // Your task is to display a renewal message when a user logs into the software system and is notified their subscription will soon end. 
 // You'll need to add a couple of decision statements to properly add branching logic to the application to satisfy the requirements.
 
+using System;
+
+class Program{
+    static void Main(string[]args){
+        Random random = new Random();
+        int daysUntilExpiration = random.Next(12);
+        int discountPercentage = 0;
+
+        if (daysUntilExpiration == 0)
+        {
+            Console.WriteLine("Your subscription has expired.");
+        }
+        else if (daysUntilExpiration == 1)
+        {
+            Console.WriteLine("Your subscription expires within a day!");
+            discountPercentage = 20;
+        }
+        else if (daysUntilExpiration <= 5)
+        {
+            Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+            discountPercentage = 10;
+        }
+        else if (daysUntilExpiration <= 10)
+        {
+            Console.WriteLine("Your subscription will expire soon. Renew now!");
+        }
+
+        if (discountPercentage > 0)
+        {
+            Console.WriteLine($"Renew now and save {discountPercentage}%.");
+        }
+    }
+}
