1b2316acbb2218670e2d792e59e9868450c69160
Josh <jthalappillil7@gmail.com>
2023-03-24 13:58:49 +1100

    Update Program.cs
    Did a small challenge to learn for loops properly and to condense and access strings

--- C#/Introduction to C#/Guided Projects/Iteration Challenge/Program.cs
+++ C#/Introduction to C#/Guided Projects/Iteration Challenge/Program.cs
@@ -38,7 +38,15 @@ class Program{
         // Code challenge - Report the Order IDs that need further investigation
         // Your team has found a pattern. Orders that start with the letter "B" encounter fraud at a rate 25 times greater than the normal rate. 
         // You'll write new code that outputs the Order ID of new orders where the Order ID starts with the letter "B". This will be used by the fraud team to investigate further.
+        string[] fraudID = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
 
+        foreach (string item in fraudID)
+        {
+            if (item.StartsWith("B"))
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
 }
 
