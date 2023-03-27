b9d159eb30f68893945233d6838d463c32db411b
Josh <jthalappillil7@gmail.com>
2023-03-27 16:26:19 +1100

    Initialised new variables
    Intialised new variables into console

---
+++ C#/Introduction to C#/Guided Projects/Console Testing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Challenge_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Hello This is a test console");
+            Console.ReadKey();
+        }
+    }
+}
