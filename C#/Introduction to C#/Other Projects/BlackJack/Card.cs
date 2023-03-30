86a95a61b932e2f1747ebe1ef1e6308c85812f16
Josh <jthalappillil7@gmail.com>
2023-03-30 15:55:12 +1100

    Created constructors with appropriate naming conventions
    Created constructors and assigned values. Included rank values to the Value variable

--- C#/Introduction to C#/Other Projects/BlackJack/Card.cs
+++ C#/Introduction to C#/Other Projects/BlackJack/Card.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace BlackJack
 {
-    internal class Card
+    public class Card
     {
+        public Suit _suit { get; }
+        public Rank _rank { get; }
+        public int Value { get; }
+
+        public Card(Suit suit, Rank rank)
+        {
+            _suit = suit;
+            _rank = rank;
+            Value = (int)_rank;
+        }
+
+        public enum Suit
+        {
+            CLUB,
+            DIAMOND,
+            HEART,
+            SPADE
+        }
+        public enum Rank
+        {
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Jack = 10,
+            Queen = 10,
+            King = 10,
+            Ace = 11
+        }
     }
 }
