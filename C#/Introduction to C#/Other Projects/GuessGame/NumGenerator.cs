693c8f4bf06b032f4bfb47dfb74d721285f11fd5
Josh <jthalappillil7@gmail.com>
2023-04-03 12:49:31 +1000

    Creation of GuessGame
    Created GuessGame first object.

---
+++ C#/Introduction to C#/Other Projects/GuessGame/NumGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessGame
+{
+    public class NumGenerator
+    {
+        public void WelcomeMsg()
+        {
+            Console.WriteLine("Welcome to GuessGame. Choose a number between 1 and 10.");
+        }
+    }
+}
---
+++ C#/Introduction to C#/Other Projects/GuessGame/Program.cs
@@ -0,0 +1,11 @@
+﻿using GuessGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+var simulate = new NumGenerator();
+simulate.WelcomeMsg();
