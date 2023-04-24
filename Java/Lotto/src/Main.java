5a62bf069e9877860395e24773808d0b23afc147
Josh <jthalappillil7@gmail.com>
2023-04-24 11:28:16 +1000

    Creation of Lotto Main.Java

---
+++ Java/Lotto/src/Main.java
@@ -0,0 +1,17 @@
+// Lotto Checker
+// Lotto numbers are drawn in the range of between 1 and 24
+// A number may be drawn one or more times in any single draw
+// This method should receive 3 number picks from a user that represent the numbers chosen by a player.
+// The numbers chosen by the player must fall within the range of the field of numbers being drawn.
+// Each time the method is called it will generate eight random numbers within the predefined range (1-24).
+// The method should return a string that informs the user of the results of the draw including the drawn numbers,
+// the numbers chosen by the player, which numbers were correct guesses by the player
+// and how many correct guesses occurred in the draw.
+// The method should also notify of any error e.g., if the number picked by the user fall outside the prescribed range.
+public class Main {
+    public static void main(String[] args){
+        public static int num(){
+
+        }
+    }
+}
