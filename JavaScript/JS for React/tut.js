085d1470e8115e974d2b44c16258ff1a7ca87d90
Josh <jthalappillil7@gmail.com>
2023-05-22 14:45:00 +1000

    Additional learning for react

---
+++ JavaScript/JS for React/tut.js
@@ -0,0 +1,14 @@
+// let age = 16;
+// let name = age > 10 ? "Pedro" : "Jack";
+
+// const Component = () => {
+//     return age > 10 ? <div>Pedro</div> : <div>Jack</div>;
+// }
+
+const person = {
+    name: "Pedro",
+    age: 20,
+    isMarried: false,
+};
+
+const { name, age, isMarried } = person;
