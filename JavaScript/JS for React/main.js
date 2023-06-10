8419464bc6846d874faad666bcbb3e3f765cdedd
Josh <jthalappillil7@gmail.com>
2023-06-10 10:08:14 +1000

    Update of JS tutorial files

--- JavaScript/JS for React/main.js
+++ JavaScript/JS for React/main.js
@@ -30,18 +30,43 @@
 
 // Async with Fetch API
 
-const loadUsers = async () => {    
-    return (await fetch("data/users.json")).json();
+// const loadUsers = async () => {    
+//     return (await fetch("data/users.json")).json();
     
-}
+// }
 
-document.addEventListener("DOMContentLoaded", async () => {
-    let users = [];
-    try {
-        users = await loadUsers();
-    } catch (e) {
-        console.log("Error");
-        console.log(e)
-    }
-    console.log(users);
-})
+// document.addEventListener("DOMContentLoaded", async () => {
+//     let users = [];
+//     try {
+//         users = await loadUsers();
+//     } catch (e) {
+//         console.log("Error");
+//         console.log(e)
+//     }
+//     console.log(users);
+// })
+
+// User input
+// let username = window.prompt("Enter a name:");
+// console.log(username)
+// let username;
+// document.getElementById("myButton").onclick = function(){
+//     username = document.getElementById("myText").value;
+//     console.log(username);
+//     document.getElementById("myLabel").innerHTML = "Hello " + username;
+// }
+
+
+
+
+document.getElementById("submitButton").onclick = function(){
+    a = document.getElementById("aTextBox").value;
+    a = Number(a)
+
+    b = document.getElementById("bTextBox").value;
+    b = Number(b)
+
+    c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    document.getElementById("cLabel").innerHTML = "Side C: " + c;
+
+}
